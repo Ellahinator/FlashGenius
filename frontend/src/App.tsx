629ba46052c07a3121a4d6b004f949d780ba9285
Ellahi <ellahinator02@gmail.com>
2023-09-18 21:35:21 -0400

    homepage + navbar implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,21 @@
-import { useEffect, useState } from "react";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react";
+import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Logo } from "./Logo";
-import axios from "axios";
 import Home from "./pages/Home";
+import Nav from "./components/Navbar";
 
 export const App = () => {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    setMessage("Loading...");
-    axios
-      .get("http://127.0.0.1:8000/")
-      .then((response) => {
-        setMessage(response.data.message);
-      })
-      .catch((error) => {
-        setMessage("Error... Make sure the backend is running!");
-        console.error("There was an error fetching data", error);
-      });
-  }, []);
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Home />
-            <Text>
-              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-            </Text>
-            <Text fontSize="2xl">Backend Response: {message}</Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={"404"} />
+          </Routes>
+        </Box>
+      </ChakraProvider>
+    </BrowserRouter>
   );
 };
