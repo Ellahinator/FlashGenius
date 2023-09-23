ce732fee233036e81221c14c14655fc41f875a53
Ellahi <ellahinator02@gmail.com>
2023-09-23 11:57:40 -0400

    improve models, use Django's built in User

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,39 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, User
 from django.contrib.auth.hashers import make_password, check_password
 
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=128)
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
+    class Meta:
+        abstract = True
 
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
-
-
-class Deck(models.Model):
+class Deck(BaseModel):
     deck_id = models.AutoField(primary_key=True)
     deck_name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='decks', on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('deck_name', 'user')
 
-class Flashcard(models.Model):
+class Flashcard(BaseModel):
     flashcard_id = models.AutoField(primary_key=True)
     term = models.TextField()
     definition = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    decks = models.ManyToManyField(Deck, through='DeckFlashcard')
-
+    user = models.ForeignKey(User, related_name='flashcards', on_delete=models.CASCADE)
 
 class DeckFlashcard(models.Model):
-    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    deck = models.ForeignKey(Deck, related_name='flashcards', on_delete=models.CASCADE)
     flashcard = models.ForeignKey(Flashcard, on_delete=models.CASCADE)
     position = models.IntegerField()
+
     class Meta:
         unique_together = ('deck', 'position')
+        index_together = ['position']
+
+    def save(self, *args, **kwargs):
+        if not self.position:
+            max_position = DeckFlashcard.objects.filter(deck=self.deck).aggregate(models.Max('position'))['position__max']
+            self.position = max_position + 1 if max_position else 1
+        super().save(*args, **kwargs)
