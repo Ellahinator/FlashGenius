307ce651ccdfc24f0b63e68f688ebd63336a046b
Berk Gülay <berkgulay@berks-mbp-2.mynetworksettings.com>
2023-11-29 13:50:51 -0500

    Fixed Login-Logout state updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,17 +9,35 @@ import Generator from "./pages/Generator";
 import FlashcardsDisplay from "./pages/FlashcardsDisplay";
 import UserDecks from "./pages/UserDecks";
 import Footer from "./components/Footer";
+import { useState , useEffect } from "react";
+import { getCookie , removeCookie, setCookie} from "typescript-cookie";
 
 export const App = () => {
+  const [token,setToken] = useState<string>('');
+  useEffect(()=> {
+    const jwt_token = getCookie("jwt_token")
+    if (jwt_token){
+      setToken(jwt_token)
+    }
+
+  }, [])
+  const logout = ()=>{
+    setToken('')
+    removeCookie("jwt_token");
+  }
+  const login = (token:string)=>{
+    setCookie("jwt_token", token);
+    setToken(token)
+  }
   return (
     <BrowserRouter>
       <ChakraProvider theme={theme}>
         <Box textAlign="center" fontSize="xl">
-          <Nav />
+          <Nav token={token} logout={logout}/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignupCard />} />
-            <Route path="/login" element={<LoginCard />} />
+            <Route path="/login" element={<LoginCard login = {login}/>} />
             <Route path="/account" element={<Account />} />
             <Route path="/generate" element={<Generator />} />
             <Route path="/deck/:deckId" element={<FlashcardsDisplay />} />
