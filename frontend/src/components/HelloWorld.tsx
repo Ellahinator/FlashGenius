524a724c547c0b169b04c9da45f10fd5b9853645
Ellahi <ellahinator02@gmail.com>
2023-12-11 16:56:29 -0500

    remove unnecessary files

--- frontend/src/components/HelloWorld.tsx
+++
@@ -1,7 +0,0 @@
-// This is just an example of a component that is not used in the app
-// but is here to show how to use the components folder
-import React from "react";
-
-export default function HelloWorld() {
-  return <div>Hello, World!</div>;
-}
